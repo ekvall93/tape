8d4a4279c147e2ab289e94cb30bf8a5823578734
ekvall93 <ekvall93>
2021-06-22 10:15:59 +0200

    add nan mean

--- tape/metrics.py
+++ tape/metrics.py
@@ -12,6 +12,45 @@ def mean_squared_error(target: Sequence[float],
     prediction_array = np.asarray(prediction)
     return np.mean(np.square(target_array - prediction_array))
 
+@registry.register_metric('spectral_angle')
+def spectral_angle(target: Sequence[float],
+                       prediction: Sequence[float]) -> float:
+
+
+    def mask_outofrange(array, lengths, mask=-1.0):
+        # dim
+        for i in range(array.shape[0]):
+            array[i, lengths[i] - 1 :, :, :, :] = mask
+        return array
+    
+    def reshape_dims(array):
+        MAX_SEQUENCE = 30
+        ION_TYPES = ["y", "b"]
+        MAX_FRAG_CHARGE = 3
+
+        n, dims = array.shape
+        assert dims == 174
+        nlosses = 1
+        return array.reshape(
+            [array.shape[0], MAX_SEQUENCE - 1, len(ION_TYPES), nlosses, MAX_FRAG_CHARGE]
+        )
+
+    def mask_outofcharge(array, charges, mask=-1.0):
+        # dim
+        for i in range(array.shape[0]):
+            if charges[i] < 3:
+                array[i, :, :, :, charges[i] :] = mask
+        return array
+    
+    def reshape_flat(array):
+        s = array.shape
+        flat_dim = [s[0], functools.reduce(lambda x, y: x * y, s[1:], 1)]
+        return array.reshape(flat_dim)
+
+
+    target_array = np.asarray(target)
+    prediction_array = np.asarray(prediction)
+    return np.mean(np.square(target_array - prediction_array))
 
 @registry.register_metric('mae')
 def mean_absolute_error(target: Sequence[float],
--- tape/models/losses.py
+++ tape/models/losses.py
@@ -2,6 +2,14 @@ import torch.nn.functional as f
 import torch
 import numpy as np
 
+
+def nanmean(v, *args, inplace=False, **kwargs):
+    if not inplace:
+        v = v.clone()
+    is_nan = torch.isnan(v)
+    v[is_nan] = 0
+    return v.sum(*args, **kwargs) / (~is_nan).float().sum(*args, **kwargs)
+
 def masked_spectral_distance(true, pred, epsilon = 1e-12):
     pred_masked = ((true + 1) * pred) / (true + 1 + epsilon)
     true_masked = ((true + 1) * true) / (true + 1 + epsilon)
@@ -13,4 +21,5 @@ def masked_spectral_distance(true, pred, epsilon = 1e-12):
     arccos = torch.acos(product)
     spectral_distance = 2 * arccos / np.pi
 
-    return spectral_distance.mean()
+    return nanmean(spectral_distance)
+
