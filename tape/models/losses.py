42c19db9ac2b15e8272f43e619918f8bd6dc96a6
ekvall93 <ekvall93>
2021-06-24 17:36:11 +0200

    try adding clip

--- tape/models/losses.py
+++ tape/models/losses.py
@@ -19,14 +19,17 @@ cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
 
 
 def masked_spectral_distance(true, pred, epsilon = 1e-7):
-    pred_masked = ((true + 1) * pred) / (true + 1 + epsilon)
-    true_masked = ((true + 1) * true) / (true + 1 + epsilon)
+    #pred_masked = ((true + 1) * pred) / (true + 1 + epsilon)
+    #true_masked = ((true + 1) * true) / (true + 1 + epsilon)
+
+    pred_masked = ((true + 1) * pred) / (torch.clamp(true + 1, min=epsilon))
+    true_masked = ((true + 1) * true) / (torch.clamp(true + 1, min=epsilon))
     
     product = cos(pred_masked, true_masked)
-    #product_clipped = torch.clamp(product, min=-1, max=1)
-    arccos = torch.acos(product)
+    product_clipped = torch.clamp(product, min=-1, max=1)
+    arccos = torch.acos(product_clipped)
     spectral_distance = 2 * arccos / np.pi
-    return torch.median(spectral_distance)
+    return torch.mean(spectral_distance)
 
 def masked_spectral_distance_old(true, pred, epsilon = 1e-7):
     pred_masked = ((true + 1) * pred) / (true + 1 + epsilon)
