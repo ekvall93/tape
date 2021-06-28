513ec07bf2be4a46d2657330cded038481d6ca38
ekvall93 <ekvall93>
2021-06-28 02:38:12 +0200

    loss

--- tape/models/modeling_utils.py
+++ tape/models/modeling_utils.py
@@ -34,11 +34,34 @@ from .file_utils import cached_path
 
 from .losses import masked_spectral_distance
 
-
+#from .modeling_lstm import ProteinLSTMModelProsit, ProteinLSTMConfig
 CONFIG_NAME = "config.json"
 WEIGHTS_NAME = "pytorch_model.bin"
 
 logger = logging.getLogger(__name__)
+
+#LSTM_PRETRAINED_MODEL_ARCHIVE_MAP: typing.Dict[str, str] = {}
+
+
+class ProteinLSTMConfig():
+    
+
+    def __init__(self,
+                 vocab_size: int = 30,
+                 input_size: int = 768,
+                 hidden_size: int = 1024,
+                 num_hidden_layers: int = 3,
+                 hidden_dropout_prob: float = 0.1,
+                 initializer_range: float = 0.02,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.vocab_size = vocab_size
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.initializer_range = initializer_range
+
 
 class ProteinLSTMLayer(nn.Module):
 
@@ -51,6 +74,106 @@ class ProteinLSTMLayer(nn.Module):
         inputs = self.dropout(inputs)
         self.lstm.flatten_parameters()
         return self.lstm(inputs)
+
+
+class ProteinLSTMPooler(nn.Module):
+    def __init__(self, num_hidden_layers, hidden_size):
+        super().__init__()
+        self.scalar_reweighting = nn.Linear(2 * num_hidden_layers, 1)
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.activation = nn.Tanh()
+
+    def forward(self, hidden_states):
+        # We "pool" the model by simply taking the hidden state corresponding
+        # to the first token.
+        pooled_output = self.scalar_reweighting(hidden_states).squeeze(2)
+        pooled_output = self.dense(pooled_output)
+        pooled_output = self.activation(pooled_output)
+        return pooled_output
+
+
+class ProteinLSTMEncoder(nn.Module):
+
+    def __init__(self, config: ProteinLSTMConfig):
+        super().__init__()
+        forward_lstm = [ProteinLSTMLayer(config.input_size, config.hidden_size)]
+        reverse_lstm = [ProteinLSTMLayer(config.input_size, config.hidden_size)]
+        for _ in range(config.num_hidden_layers - 1):
+            forward_lstm.append(ProteinLSTMLayer(
+                config.hidden_size, config.hidden_size, config.hidden_dropout_prob))
+            reverse_lstm.append(ProteinLSTMLayer(
+                config.hidden_size, config.hidden_size, config.hidden_dropout_prob))
+        self.forward_lstm = nn.ModuleList(forward_lstm)
+        self.reverse_lstm = nn.ModuleList(reverse_lstm)
+        self.output_hidden_states = None
+
+    def forward(self, inputs, input_mask=None):
+        all_forward_pooled = ()
+        all_reverse_pooled = ()
+        all_hidden_states = (inputs,)
+        forward_output = inputs
+        for layer in self.forward_lstm:
+            forward_output, forward_pooled = layer(forward_output)
+            all_forward_pooled = all_forward_pooled + (forward_pooled[0],)
+            all_hidden_states = all_hidden_states + (forward_output,)
+
+        reversed_sequence = self.reverse_sequence(inputs, input_mask)
+        reverse_output = reversed_sequence
+        for layer in self.reverse_lstm:
+            reverse_output, reverse_pooled = layer(reverse_output)
+            all_reverse_pooled = all_reverse_pooled + (reverse_pooled[0],)
+            all_hidden_states = all_hidden_states + (reverse_output,)
+        reverse_output = self.reverse_sequence(reverse_output, input_mask)
+
+        output = torch.cat((forward_output, reverse_output), dim=2)
+        pooled = all_forward_pooled + all_reverse_pooled
+        pooled = torch.stack(pooled, 3).squeeze(0)
+        outputs = (output, pooled)
+        if self.output_hidden_states:
+            outputs = outputs + (all_hidden_states,)
+
+        return outputs  # sequence_embedding, pooled_embedding, (hidden_states)
+
+    def reverse_sequence(self, sequence, input_mask):
+        if input_mask is None:
+            idx = torch.arange(sequence.size(1) - 1, -1, -1).cuda()
+            reversed_sequence = sequence.index_select(1, idx)
+        else:
+            sequence_lengths = input_mask.sum(1)
+            reversed_sequence = []
+            for seq, seqlen in zip(sequence, sequence_lengths):
+                idx = torch.arange(seqlen - 1, -1, -1).cuda()
+                seq = seq.index_select(0, idx)
+                seq = F.pad(seq, [0, 0, 0, sequence.size(1) - seqlen])
+                reversed_sequence.append(seq)
+            reversed_sequence = torch.stack(reversed_sequence, 0)
+        return reversed_sequence
+
+
+
+class ProteinLSTMModelProsit(nn.Module):
+
+    def __init__(self, config: ProteinLSTMConfig):
+        super().__init__()
+        #super().__init__(config)
+        #self.embed_matrix = nn.Embedding(config.vocab_size, config.input_size)
+        self.encoder = ProteinLSTMEncoder(config)
+        self.pooler = ProteinLSTMPooler(config)
+        #self.output_hidden_states = config.output_hidden_states
+        #self.init_weights()
+
+    def forward(self, sequence, input_mask=None):
+        if input_mask is None:
+            input_mask = torch.ones_like(sequence)
+
+        # fp16 compatibility
+        #embedding_output = self.embed_matrix(input_ids)
+        outputs = self.encoder(sequence, input_mask=None)
+        sequence_output = outputs[0]
+        pooled_outputs = self.pooler(outputs[1])
+
+        outputs = (sequence_output, pooled_outputs) + outputs[2:]
+        return outputs  # sequence_output, pooled_output, (hidden_states)
 
 class ProteinConfig(object):
     """ Base class for all configuration classes.
@@ -836,9 +959,11 @@ class ValuePredictionHead(nn.Module):
 class ValuePredictionHeadPrositFragmentation(nn.Module):
     def __init__(self, hidden_size: int, out:int, dropout: float = 0.):
         super().__init__()
-        self.value_prediction = SimpleMLP(512, 512, out, dropout, True)
-        self.meta_dense = SimpleLinear(7, hidden_size, 0.1, False)
+        self.value_prediction = SimpleMLP(512, 256, out, 0.3, True)
+        self.meta_dense = SimpleLinear(7, hidden_size, 0.3, False)
         self.G = ProteinLSTMLayer(768, 512)
+        self.pooler = ProteinLSTMPooler(256, 1)
+        #self.decode = ProteinLSTMModelProsit(ProteinLSTMConfig())
 
     def forward(self, sequence_output, meta_data, targets=None):
         meta = self.meta_dense(meta_data)
@@ -849,11 +974,15 @@ class ValuePredictionHeadPrositFragmentation(nn.Module):
         x = meta[:,None,:] * sequence_output
         #print(x.shape)
         
-
+        #outputs = self.decode(x)
+        #sequence_output, pooled_output = outputs[:2]
         #X_tmp = x.unsqueeze(2).repeat(1, 29, 1)
         
         sequence, pooled_out = self.G(x)
+        #y = self.pooler(pooled_out[1])
         y = pooled_out[0].squeeze()
+        #print(y.shape)
+        #print(sequence.shape)
         #print(sequence.shape)
         #print(pooled_out[0].squeeze().shape)
         #y = torch.repeat_interleave(x, 29, 2)
