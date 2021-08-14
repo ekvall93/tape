b5d50d44b5475e8e20a1847536a6799c06fdbc99
ekvall93 <ekvall93>
2021-08-14 22:34:50 +0200

    try bucket

--- tape/utils/setup_utils.py
+++ tape/utils/setup_utils.py
@@ -7,7 +7,7 @@ import sys
 
 import torch
 import torch.distributed as dist
-from torch.utils.data import DataLoader, RandomSampler, Dataset, SequentialSampler
+from torch.utils.data import DataLoader, RandomSampler, Dataset, SequentialSampler, BatchSampler
 from torch.utils.data.distributed import DistributedSampler
 from ..optimization import AdamW
 
@@ -118,7 +118,7 @@ def setup_loader(dataset: Dataset,
             batch_size, local_rank, n_gpu, gradient_accumulation_steps) * n_gpu
 
     if not shuffle_dataset and isEval:
-        batch_sampler = SequentialSampler(dataset, batch_size=batch_size)
+        batch_sampler = BatchSampler(SequentialSampler(dataset), batch_size=batch_size)
     else:
         sampler = DistributedSampler(dataset) if local_rank != -1 else RandomSampler(dataset)
         # WARNING: this will fail if the primary sequence is not the first thing the dataset returns
